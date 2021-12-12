a740c330e345dccb38923b002ebde94cc73f1bd0
Ondra Lengal <ondra.lengal@gmail.com>
2021-12-12 20:37:34 +0100

    add --repeat

--- pcap-generator.py
+++ pcap-generator.py
@@ -17,7 +17,7 @@ FIN_ACK_FLAG = FIN_FLAG + ACK_FLAG
 SRC_IP = "100.0.0.1"
 DST_IP = "172.16.0.1"
 
-def createPcapFile(data, outfile):
+def createPcapFile(data, outfile, repeat=1):
     pkts = []
 
     ip_send = IP(src=SRC_IP, dst=DST_IP)
@@ -29,19 +29,20 @@ def createPcapFile(data, outfile):
     pkts += ip_send / TCP(sport=SPORT, dport=DPORT, flags=ACK_FLAG, seq=1, ack=1)
 
     seq = 1
-    data_index = 0
-    # do TCP segmentation for packets larger than MAX_RAWDATA_SIZE
-    while data_index < len(data):
-        tcp = TCP(sport=SPORT, dport=DPORT, flags=ACK_FLAG, seq=seq, ack=1)
-        data_end = min(data_index + MAX_RAWDATA_SIZE, len(data))
-        payload = data[data_index : data_end]
-        # print(f"data start = {data_index}, data end = {data_end}")
-        app = Raw(payload)
-        pkt = ip_send / tcp / app
-        pkts += pkt
-        offset = data_end - data_index
-        seq += offset
-        data_index += offset
+    for i in range(0,repeat):
+        data_index = 0
+        # do TCP segmentation for packets larger than MAX_RAWDATA_SIZE
+        while data_index < len(data):
+            tcp = TCP(sport=SPORT, dport=DPORT, flags=ACK_FLAG, seq=seq, ack=1)
+            data_end = min(data_index + MAX_RAWDATA_SIZE, len(data))
+            payload = data[data_index : data_end]
+            # print(f"data start = {data_index}, data end = {data_end}")
+            app = Raw(payload)
+            pkt = ip_send / tcp / app
+            pkts += pkt
+            offset = data_end - data_index
+            seq += offset
+            data_index += offset
 
     # bulk ACK from receiver
     pkts += ip_recv / TCP(sport=DPORT, dport=SPORT, flags=ACK_FLAG, seq=1, ack=seq)
@@ -60,11 +61,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PCAP generator for ReDoS strings')
     parser.add_argument('input', metavar='<input.txt>')
     parser.add_argument('output', metavar='<output.pcap>')
+    parser.add_argument('-r', '--repeat', type=int, metavar='N', default=1,
+        help='repeat input N times (default: %(default)s)')
     args = parser.parse_args()
 
     with open(args.input, "rb") as fd:
         data = fd.read()
         print(f'length = {len(data)}')
-        createPcapFile(data, args.output)
+        createPcapFile(data, args.output, repeat=args.repeat)
 
     pass
